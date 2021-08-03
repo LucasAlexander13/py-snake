a0aaedebaec53fb4bb5d3b9aa9338c69b531de6c
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-03 11:08:29 -0300

    Added functions

---
+++ functions.py
@@ -0,0 +1,7 @@
+from snake import *
+
+def clear(): screen.bye()
+def move_up(): segment.setheading(90)
+def move_left(): segment.setheading(180)
+def move_right(): segment.setheading(0)
+def move_down(): segment.setheading(270)
