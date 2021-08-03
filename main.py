627dea3897b90ec53ec6776539c11b7627915ac6
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-03 11:50:21 -0300

    Some fixes in snake segments movement

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ playing = True
 
 while playing:
     try:
-        sleep(0.2)
-        for segment in snake:
-            segment.forward(20)
-
         screen.update()
+        sleep(0.2)
+        
+        for segment in range(len(snake) - 1, 0, -1):
+            new_x = snake[segment - 1].xcor()
+            new_y = snake[segment - 1].ycor()
+            snake[segment].goto(new_x, new_y)
+        
+        move_snake()
+
     except:
         break
