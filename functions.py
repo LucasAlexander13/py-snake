70a31e0efa99483fbed7dce0f329c6a1950c021f
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-03 11:49:30 -0300

    Added move_snake function using onkey method

--- functions.py
+++ functions.py
@@ -1,7 +1,24 @@
 from snake import *
 
 def clear(): screen.bye()
-def move_up(): segment.setheading(90)
-def move_left(): segment.setheading(180)
-def move_right(): segment.setheading(0)
-def move_down(): segment.setheading(270)
+
+def move_up(): 
+    snake_head.setheading(90)
+
+def move_left(): 
+    snake_head.setheading(180)
+
+def move_right(): 
+    snake_head.setheading(0)
+
+def move_down(): 
+    snake_head.setheading(270)
+
+def move_snake():
+    screen.listen()
+    screen.onkey(move_up, 'w')
+    screen.onkey(move_left, 'a')
+    screen.onkey(move_right, 'd')
+    screen.onkey(move_down, 's')
+    screen.onkey(clear, 'c')
+    snake_head.forward(20)
