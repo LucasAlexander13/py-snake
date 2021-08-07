5b820107050a330e077af4678e4db4c24dc8284e
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-07 09:18:02 -0300

    Added dot to screen

--- score.py
+++ score.py
@@ -1,9 +1,22 @@
 from turtle import Turtle
+from random import randint, choice
 
 color = ['orange', 'dark orange', 'firebrick', 'dark red', 'crimson', 'medium violet red', 'dark magenta', 'medium orchid', 'medium purple', 'indigo', 'slate blue']
 class Score():
-    def __init__(self):
+    def __init__(self, snake):
         self.dot = Turtle()
         self.dot.hideturtle()
         self.dot.penup()
+        self.set_position(snake)
         self.points = 0
+
+    def set_position(self, snake):
+        random_x = randint(-32, 32) * 10
+        random_y = randint(-24, 24) * 10
+        self.dot.goto(random_x, random_y)
+        for segment in snake.segments:
+            if self.dot.position == segment.position:
+                return self.set_position(snake)
+        self.dot.pendown()
+        self.dot.dot(20, choice(color))
+        self.dot.penup()
