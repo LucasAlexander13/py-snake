be027be562fe641ae887f7fb40b7a1a2b3435134
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-07 09:14:43 -0300

    Added initial score method

--- score.py
+++ score.py
@@ -3,4 +3,7 @@ from turtle import Turtle
 color = ['orange', 'dark orange', 'firebrick', 'dark red', 'crimson', 'medium violet red', 'dark magenta', 'medium orchid', 'medium purple', 'indigo', 'slate blue']
 class Score():
     def __init__(self):
-        pass
+        self.dot = Turtle()
+        self.dot.hideturtle()
+        self.dot.penup()
+        self.points = 0
