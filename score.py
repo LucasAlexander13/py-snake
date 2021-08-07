1c2c1eac442b0e38deed9164086ade4ec5fed1de
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-07 09:13:48 -0300

    Added colors to dot

--- score.py
+++ score.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+color = ['orange', 'dark orange', 'firebrick', 'dark red', 'crimson', 'medium violet red', 'dark magenta', 'medium orchid', 'medium purple', 'indigo', 'slate blue']
 class Score():
     def __init__(self):
         pass
