8ea9d640cfeb232be09314b8ecff03c3f02ae8ab
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-08 11:00:37 -0300

    Added random color method

--- food.py
+++ food.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
+from random import choice
 
+color = ['orange', 'dark orange', 'firebrick', 'dark red', 'crimson', 'medium violet red', 'dark magenta', 'medium orchid', 'medium purple', 'indigo', 'slate blue']
 class Food(Turtle):
     def __init__(self):
         super().__init__()
@@ -7,3 +9,8 @@ class Food(Turtle):
         self.penup()
         self.shapesize(8.5, 8.5)
         self.speed('fastest')
+        self.pick_color()
+
+    def pick_color(self):
+        random_color = choice(color)
+        self.color(random_color)
