8be3b7ba7edfaca5e11a03ecd3623389d702dedc
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-09 08:22:54 -0300

    Spawn food in random position

--- food.py
+++ food.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-from random import choice
+from random import choice, randint
 
 color = ['orange', 'dark orange', 'firebrick', 'dark red', 'crimson', 'medium violet red', 'dark magenta', 'medium orchid', 'medium purple', 'indigo', 'slate blue']
 class Food(Turtle):
@@ -10,7 +10,13 @@ class Food(Turtle):
         self.shapesize(0.5, 0.5)
         self.speed('fastest')
         self.pick_color()
+        self.random_spawn()
 
     def pick_color(self):
         random_color = choice(color)
         self.color(random_color)
+    
+    def random_spawn(self):
+        x_random = randint(-200, 200)
+        y_random = randint(-200, 200)
+        self.goto(x_random, y_random)
