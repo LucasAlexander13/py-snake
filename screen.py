946cf23e66d738a528487bcbcaeab6e9a772c27c
Lucas Alexander <lucasalexander13@hotmail.com>
2021-08-11 14:22:08 -0300

    Create two functions to screen module

--- screen.py
+++ screen.py
@@ -1,25 +1,23 @@
 from turtle import Screen, Turtle
 
 screen = Screen()
-screen.screensize(500, 500) 
-screen.bgcolor('black')
-screen.title('PySnake')
-screen.tracer(0)
 
-board = Turtle()
-board.penup()
-board.color('firebrick')
-board.goto(220, 220)
+def set_screen(screen):
+    screen.screensize(500, 500) 
+    screen.bgcolor('black')
+    screen.title('PySnake')
+    screen.tracer(0)
 
-# draw the edge line of the game
-board.pendown()
-board.goto(220, -220)
-board.goto(-220, -220)
-board.goto(-220, 220)
-board.goto(220, 220)
-board.hideturtle()
+def draw_edge():
+    board = Turtle()
+    board.penup()
+    board.color('firebrick')
+    board.goto(220, 220)
 
-
-
-
-
+    # draw the edge line of the game
+    board.pendown()
+    board.goto(220, -220)
+    board.goto(-220, -220)
+    board.goto(-220, 220)
+    board.goto(220, 220)
+    board.hideturtle()
